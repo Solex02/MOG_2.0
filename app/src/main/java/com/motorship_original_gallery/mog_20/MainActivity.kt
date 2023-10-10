3b3414c381333d4a5e903822b4886917370baca4
Mario <mario_barea_m-tsapp1ma2223@cev.com>
2023-10-10 09:07:56 +0200

    Menu de botones

--- app/src/main/java/com/motorship_original_gallery/mog_20/MainActivity.kt
+++ app/src/main/java/com/motorship_original_gallery/mog_20/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.motorship_original_gallery.mog_20
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            Boton(item)
+            true
+        }
+    }
+
+    fun Boton(item: MenuItem) {
+        var intent: Intent? = null
+
+        when (item.itemId) {
+            R.id.action_item1 -> intent = Intent(this, MenuPrincipal::class.java)
+            R.id.action_item2 -> intent = Intent(this, MenuPrincipal::class.java)
+            R.id.action_item3 -> intent = Intent(this, MenuPrincipal::class.java)
+            R.id.action_item4 -> intent = Intent(this, MenuPrincipal::class.java)
+            R.id.action_item5 -> intent = Intent(this, MenuPrincipal::class.java)
+        }
+
+        if (intent != null) {
+            startActivity(intent)
+        }
     }
 }
